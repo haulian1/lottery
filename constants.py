87dd2500fcccc2c4afcdd8403fe3d067c0279afa
Hau Lian <haulian@haus-air.lan>
2024-11-08 12:55:38 -0500

    adding constants and helpers file and some inital code to those

---
+++ constants.py
@@ -0,0 +1,37 @@
+import pytz
+
+class Constant:
+	BATCH_SIZE = 2000000
+	class DateTime:
+		TIME_ZONE = pytz.timezone("America/New_York")
+
+	
+	class Modes:
+		POWERBALL = 'POWER'
+		MEGAMILLION = 'MEGA'
+
+
+	class Regex:
+		MEGAMILLION = r"([0][1-9]|[1-6][0-9]|70){5}([0-1][1-9]|[2][0-5])"
+		POWERBALL = r"([0][1-9]|[1-6][0-9]){5}([0-1][1-9]|[2][0-5])"
+
+
+	class Mega:
+		WHITE_BALL_LIMIT = 70
+		SPECIAL_BALL_LIMIT = 25
+		DAY_OFFSETS = [(1,1), (0,3), (2,2), (1,1), (0,4), (3,3), (2,2)]
+
+
+	class Power:
+		WHITE_BALL_LIMIT = 69
+		SPECIAL_BALL_LIMIT = 26
+		DAY_OFFSETS = [(0,2), (1,1), (0,3), (2,2), (1,1), (0,2), (1,1)]
+
+	class Error:
+		EXIT_STATUS = {
+		'get_days_offset': 1
+		}
+
+
+
+			
---
+++ helpers.py
@@ -0,0 +1,12 @@
+import constants
+from datetime import datetime
+
+'''
+
+'''
+def get_days_offset(cur_week_day: int, cur_hr: int) -> int:
+	
+	pass
+
+def get_next_lotto_date(datetime: datetime):
+	pass
